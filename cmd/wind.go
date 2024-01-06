41fff2909751cb68ef93e42e00ed43eca105be7f
gregoireLeBras <lebras.gregoire@gmail.com>
2024-01-06 15:34:43 +0100

    remove coms

--- cmd/wind.go
+++ cmd/wind.go
@@ -9,34 +9,10 @@ import (
 	internal "imt-atlantique.project.group.fr/meteo-airport/internal"
 )
 
-/*func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(brokerURI)
-	// opts.SetUsername(username)
-	// opts.SetPassword(password)
-	opts.SetClientID(clientId)
-	return opts
-}
-
-func connect(brokerURI string, clientId string) mqtt.Client {
-	opts := createClientOptions(brokerURI, clientId)
-	client := mqtt.NewClient(opts)
-	token := client.Connect()
-	for !token.WaitTimeout(3 * time.Second) {
-	}
-	if err := token.Error(); err != nil {
-		panic(err)
-	}
-	return client
-}*/
-
 func main() {
 	client := internal.MakeDefaultClient()
 	// MQTT broker configuration
-	/*brokerURI := "mqtt://411d6c045163486b846c891f3910e83f.s2.eu.hivemq.cloud:1883" //maybe tcp://localhost:1883
-	clientID := "greg_client"
 
-	client := connect(brokerURI, clientID)*/
 	actualWind := 40
 	min := 10
 	max := 120
