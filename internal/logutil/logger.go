35d64cc9c4b69eb4615c50e38139186962a98445
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-01-07 11:09:28 +0100

    chore(logger): improve logger for write the line and the caller on error or warning

--- internal/logutil/logger.go
+++ internal/logutil/logger.go
@@ -3,36 +3,41 @@ package logutil
 import (
 	"fmt"
 	"github.com/fatih/color"
-	"io"
 	"log"
 	"os"
+	"runtime"
 )
 
 var logger = log.New(os.Stdout, "METEO-AIRPORT", log.LstdFlags)
-var formatOutput = "\n\t%s %s\n"
 
 var (
 	red    = color.New(color.FgRed).SprintFunc()
-	green  = color.New(color.FgGreen).SprintFunc()
 	yellow = color.New(color.FgYellow).SprintFunc()
 	bold   = color.New(color.Bold).SprintFunc()
+	green  = color.New(color.FgGreen).SprintFunc()
+	reset  = color.New(color.Reset).SprintFunc()
 )
 
-func Error(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a...)
-	logger.Printf(formatOutput, red(bold("[ERROR]")), red(msg))
+// logMessage formats the log message with the file name and line number
+func logMessage(message, level string) string {
+	_, file, line, ok := runtime.Caller(2)
+	if ok && (level == red(bold("[Error] ")) || level == yellow(bold("[Warn] "))) {
+		return fmt.Sprintf("\n\t%s%s:%d %s%s", level, file, line, message, reset())
+	}
+	return fmt.Sprintf("\n\t%s%s%s", level, message, reset())
 }
 
-func Info(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a...)
-	logger.Printf(formatOutput, green(bold("[INFO]")), green(msg))
+// Error logs an error message with red color
+func Error(format string, v ...interface{}) {
+	logger.Print(logMessage(red(fmt.Sprintf(format, v...)), red(bold("[Error] "))))
 }
 
-func Warn(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a...)
-	logger.Printf(formatOutput, yellow(bold("[WARN]")), yellow(msg))
+// Warn logs a warning message with yellow color
+func Warn(format string, v ...interface{}) {
+	logger.Print(logMessage(yellow(fmt.Sprintf(format, v...)), yellow(bold("[Warn] "))))
 }
 
-func SetOutput(w io.Writer) {
-	logger.SetOutput(w)
+// Info logs an info message with bold formatting
+func Info(format string, v ...interface{}) {
+	logger.Print(logMessage(green(fmt.Sprintf(format, v...)), green(bold("[Info] "))))
 }
