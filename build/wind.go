a0153c271f39bb95f16794d2eacce08549ddd92a
gregoireLeBras <lebras.gregoire@gmail.com>
2024-01-13 13:38:30 +0100

    data generation and publish in two different functions

--- build/wind.go
+++ build/wind.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"imt-atlantique.project.group.fr/meteo-airport/internal/logutil"
 	"imt-atlantique.project.group.fr/meteo-airport/internal/sensor"
 	"math/rand"
 	"time"
@@ -9,35 +10,54 @@ import (
 	mqtt_helper "imt-atlantique.project.group.fr/meteo-airport/internal/mqtt_helper"
 )
 
+func windDataGeneration(actualWind float64, min float64, max float64) float64 {
+	actualWind = actualWind + (rand.Float64()-rand.Float64())*5
+
+	if actualWind < min {
+		actualWind = min
+	}
+	if actualWind > max {
+		actualWind = max
+	}
+
+	return actualWind
+}
+
+func publishData(actualWind float64, client *mqtt_helper.MQTTClient) {
+
+	data := sensor.Measurement{
+		SensorID:  2,
+		AirportID: "CDG",
+		Type:      "Wind speed",
+		Value:     actualWind,
+		Timestamp: time.Now(),
+	}
+
+	err := data.PublishOnMQTT(2, false, client)
+	if err != nil {
+		logutil.Error(fmt.Sprintf("Failed to publish data to client: %v", err))
+	}
+}
+
 func main() {
 	if config, err := mqtt_helper.RetrieveMQTTPropertiesFromYaml("./config/hiveClientConfig.yaml"); err != nil {
 		panic(err)
 	} else {
 		client := mqtt_helper.NewClient(config, "clientId")
-		client.Connect()
+
+		err := client.Connect()
+		if err != nil {
+			logutil.Error(fmt.Sprintf("Cannot connect to client: %v", err))
+		}
 
 		actualWind := 40.0
-		min := 10.0
-		max := 120.0
+		minimalValue := 10.0
+		maximalValue := 120.0
 
 		for {
-			actualWind = actualWind + (rand.Float64()-rand.Float64())*5
-			if actualWind < min {
-				actualWind = min
-			}
-			if actualWind > max {
-				actualWind = max
-			}
-			data := sensor.Measurement{
-				SensorID:  2,
-				AirportID: "CDG",
-				Type:      "Wind speed",
-				Value:     actualWind,
-				Timestamp: time.Now(),
-			}
 
-			data.PublishOnMQTT(2, false, client)
-			fmt.Printf("%#v", data)
+			actualWind := windDataGeneration(actualWind, minimalValue, maximalValue)
+			publishData(actualWind, client)
 
 			time.Sleep(5 * time.Second)
 		}
