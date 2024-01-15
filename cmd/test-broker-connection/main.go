bdda8016550c8de7736e379a0da475b7596b1711
gregoireLeBras <lebras.gregoire@gmail.com>
2024-01-15 22:06:36 +0100

    mqqt github to pahoMqtt

--- cmd/test-broker-connection/main.go
+++ cmd/test-broker-connection/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"imt-atlantique.project.group.fr/meteo-airport/internal/mqtt_helper"
+	pahoMqtt "github.com/eclipse/paho.mqtt.golang"
+	"imt-atlantique.project.group.fr/meteo-airport/internal/mqtt"
 )
 
 func main() {
-	if config, err := mqtt_helper.RetrieveMQTTPropertiesFromYaml("./config/hiveClientConfig.yaml"); err != nil {
+	if config, err := mqtt.RetrieveMQTTPropertiesFromYaml("./config/hiveClientConfig.yaml"); err != nil {
 		panic(err)
 	} else {
-		client := mqtt_helper.NewClient(config, "aClientId")
+		client := mqtt.NewClient(config, "aClientId")
 		if err := client.Connect(); err != nil {
 			panic(err)
 		}
 		defer client.Disconnect()
 
 		// Print the message when it is received
-		if err := client.Subscribe("test", 1, func(client mqtt.Client, message mqtt.Message) {
+		if err := client.Subscribe("test", 1, func(client mqtt.Client, message pahoMqtt.Message) {
 			println(string(message.Payload()))
 		}); err != nil {
 			panic(err)
